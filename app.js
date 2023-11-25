f5bfeb6d7056bc6be28265f3b6852f3084e5504d
Mariam Eneji <enejimariam1997@gmail.com>
2023-11-25 09:48:46 +0100

    updates

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// As a merchant, when I click on the notification bell, I see a dropdown panel that shows me an empty list of notifications since I have none yet. When I click on the notification bell again, this dropdown panel is closed.
+
+
+
+
 let notif = document.getElementById("notif");
 let alerts = document.getElementById("alerts");
 
@@ -5,6 +10,8 @@ let alerts = document.getElementById("alerts");
 notif.addEventListener("click", function() {
     alerts.classList.toggle("hidden");
 })
+
+// As a merchant, when I click on the name of my store Davii Collections, or on my profile image placeholder DC on the far right of the topbar menu, I see a menu with a list of menu items as specified by the Figma design. Clicking on this button again closes the menu. Finally, when I click on any of the menu items in this menu, I am redirected toadmin.shopify.com
 
 
 let dcDropdown = document.getElementById("dcDropdown");
@@ -20,12 +27,17 @@ dcButton2.addEventListener("click", function() {
     dcDropdown.classList.toggle("hidden")
 });
 
+
+// As a merchant, when I click on the dismiss button of the trial callout, the trial callout is immediately removed from the page.
+
 let closed = document.getElementById("close");
 let extend = document.getElementById("extended");
 
 closed.addEventListener("click", function(){
     extend.classList.add("hidden");
 })
+
+// As a merchant, when I click the "Arrow Up" icon on the top right of the Setup guide card, it closes the card. When I click again, it reopens the card.
 
 
 let accordion1btn = document.getElementById('accordion1btn')
@@ -46,8 +58,12 @@ accordion1btn1.addEventListener("click", function() {
 })
 
 
+
+// As a merchant, when I click on any of the 5 onboarding steps, it expands the panel, showing the content of the onboarding step, and closes the previously opened one. When I click on an opened onboarding step, nothing happens.
+
 let acc = document.getElementsByClassName("accordionItems");
 let oneBottom = document.getElementsByClassName("abottom");
+let oneBottomImg = document.getElementsByClassName("abottom-img");
 
 for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener('click', function(){
@@ -56,9 +72,18 @@ for (let i = 0; i < acc.length; i++) {
             oneBottom[j].classList.remove('active');
         }
         oneBottom[i].classList.add('active')
+
+        for (let k = 0; k < oneBottomImg.length; k++) {
+            oneBottomImg[k].classList.remove('active');
+        }
+
+        oneBottomImg[i].classList.add('active')
     });
   }
 
+
+
+// As a merchant, when I click on the circle checkbox (empty) on the left side of an onboarding step title, it marks that step as completed, and expands the next incomplete step. If I click again, it marks the step as incomplete. As I mark steps as completed or incomplete, I see the onboarding progress bar showing the correct progress.
 
 
 
